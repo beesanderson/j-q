b7565c9d74070564b80eddba83e38bc21a340134
beesanderson <87204074+beesanderson@users.noreply.github.com>
2022-02-22 14:35:19 -0600

    c02.etc

---
+++ c02/js/update-array.js
@@ -0,0 +1,12 @@
+// Create the Array
+var colors = ['white', 'black', 'custom'];
+
+// Update the third item in the array
+colors[2] = 'beige'; 
+
+//Get the element with an id of colors
+var el = document.getElementById('colors')
+
+//Replace with the third item from the array
+el.textContent = colors[2];
+
