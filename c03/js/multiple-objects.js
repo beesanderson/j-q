7a17a901c4c585cbb0cbf9394e127c64e42d56c9
beesanderson <87204074+beesanderson@users.noreply.github.com>
2022-02-25 17:56:14 -0600

    EOD 2.25.22

---
+++ c03/js/multiple-objects.js
@@ -0,0 +1,10 @@
+// Creating Multiple Objects with Constructor Function
+
+function Hotel(name, rooms, booked) { //note capital letter for Object
+    this.name = name;
+    this.rooms = rooms;
+    this.booked = booked;
+    this.checkAvailability = function() {
+        return this.rooms - this.booked;
+    };
+} // no semi colon
