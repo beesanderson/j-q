7d3d32582ff0a08f258516f1fcfe5354b08c6f8a
beesanderson <87204074+beesanderson@users.noreply.github.com>
2022-03-10 13:12:32 -0600

    wip

--- c03/js/multiple-objects.js
+++ c03/js/multiple-objects.js
@@ -7,4 +7,4 @@ function Hotel(name, rooms, booked) { //note capital letter for Object
     this.checkAvailability = function() {
         return this.rooms - this.booked;
     };
-} // no semi colon
+} // no semi colon as it is the end of a code block
